7fa3abc78067aab99c84157162755d2e8e75c97b
Manish Shrivastava <mashri@microsoft.com>
2016-06-12 13:10:41 +0530

    Project Euler 2nd prob

---
+++ p2/Source.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int prev1 = 2;
+	int prev2 = 1;
+	int sum = 2;
+	while (prev2 + prev1 <= 4000000)
+	{
+		int temp = prev2 + prev1;
+		if (temp % 2 == 0)
+		{
+			sum += temp;
+		}
+		prev2 = prev1;
+		prev1 = temp;
+	}
+	printf("%d\n", sum);
+	while (1);
+}
